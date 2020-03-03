466c62a273e49ae23d396bbf36b8c2f45c1342ee
naughtyremz <naughty.remz@gmail.com>
2020-03-03 16:13:35 +0100

    delamrd

--- src/Block.js
+++ src/Block.js
@@ -2,23 +2,33 @@ import React from 'react';
 import './App.css';
 
 class Block extends React.Component{
-render(){
-    return(
-        <div>
+    state = {
+        titre: "",
+    }
+    setTitre = (event) => {
+        // console.log(event.currentTarget.value);
+        // const value = event.currentTarget.value;
+        // this.setState({titre : value});
+        // alert(value);
+        console.log("test");
+    }
+    render(){
+        return(
             <div>
-                <label>Titre du block</label> <textarea></textarea>
-                <label>Contenu du block</label> <textarea></textarea>
+                <div>
+                    <label>Titre du block</label> <input onChange={this.setTitre} value={this.state.titre}/>
+                    <label>Contenu du block</label> <textarea></textarea>
+                </div>
+                <div>
+                    <p>Numéro du block : 1</p>
+                    <p>Numéro du block suivant: 2</p>
+                </div>
+                <div>
+                    <button>Valider</button> <button>Ajouter une action(création d'un new block)</button>
+                </div>
             </div>
-            <div>
-                <p>Numéro du block : 1</p>
-                <p>Numéro du block suivant: 2</p>
-            </div>
-            <div>
-                <button>Valider</button> <button>Ajouter une action(création d'un new block)</button>
-            </div>
-        </div>
-    )
-}
+        )
+    }
 }
 
 
