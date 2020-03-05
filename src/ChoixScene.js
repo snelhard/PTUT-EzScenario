bec5c73f4ce9415d9830edc5d3175411baabb050
neibeauf <60102794+neibeauf@users.noreply.github.com>
2020-03-05 17:06:33 +0100

    fabien

--- src/ChoixScene.js
+++ src/ChoixScene.js
@@ -5,19 +5,18 @@ class ChoixScene extends React.Component{
     state = {
         id: 1,
         texte: "",
-        idSuivant:2
+        idSuivant:0
      }
      handleChangeTexteNouveauChoix = (event) => {
         const value = event.currentTarget.value;
         this.setState({texte : value});
-        this.sendData(1)
-        console.log(this.state.texte);
+        //this.sendData();
     }
     handleChangeidSuivant = (event) => {
-        const value = event.currentTarget.value;
+        var value = event.currentTarget.value;
         this.setState({idSuivant : value});
-        this.sendData(2)
-        console.log(this.state.idSuivant);
+        //this.sendData();
+
     }
     render(){
         return(
@@ -27,18 +26,19 @@ class ChoixScene extends React.Component{
                         <label>Texte choix:</label><input type="text" onChange={this.handleChangeTexteNouveauChoix} value={this.state.texte}/>
                         <label>Id Scene:</label><input type="number" onChange={this.handleChangeidSuivant} value={this.state.idSuivant}/>
                     </div>
+                    <button>valider</button> 
                 </form>
             </div>
 
         )
     }
-    sendData = (indice) => {
-        if (indice === 1) {
-            this.props.parentCallback(this.state.texte);
-        }
-        if (indice === 2){
-            this.props.parentCallback(this.state.idSuivant);
-        }
+    handleSubmitChoix=(event)=> {
+        event.preventDefault();
+        this.sendData();
+    }
+    sendData =() => {
+           this.props.parentCallback(this.state);
+           //console.log(this.state)
     }
 }
 
