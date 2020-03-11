78e3ca5e4cc2d4783ea9f9f34491b417751addce
naughtyremz <naughty.remz@gmail.com>
2020-03-11 16:19:20 +0100

    add block

--- src/Page.js
+++ src/Page.js
@@ -5,13 +5,24 @@ import Block from './Block.js'
 class Page extends React.Component{
     state = {
         titre: "La page",
+        scenes: [
+
+        ]
+    }
+    addBlock = () => {
+        var scenes = this.state.scenes.slice();
+        scenes.push({id: 1, titre: "", text: ""});
+        this.setState({scenes: scenes});
     }
     render(){
         return(
         <div className="game">
             <h1>{this.state.titre}</h1>
+            <button onClick={this.addBlock}>Ajouter scene</button>
             <div className="game-board">
-                <Block />
+                {this.state.scenes.map((scene) => (
+                    <Block />
+                ))}
             </div>
         </div>
         )
