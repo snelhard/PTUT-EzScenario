a8850b51d25873e2a44c741d373ecf8ee554f721
snelhard <43958032+snelhard@users.noreply.github.com>
2020-03-12 15:12:02 +0100

    page json

--- src/Page.js
+++ src/Page.js
@@ -18,6 +18,7 @@ class Page extends React.Component{
         <div className="game">
             <h1>{this.state.titre}</h1>
             <button onClick={this.addBlock}>Ajouter scene</button>
+            <button onClick={this.downloadJsonFile}>Fichier Json</button>
             <div className="game-board">
                 {this.state.scenes.map((scene) => (
                     <Block />
@@ -25,6 +26,36 @@ class Page extends React.Component{
             </div>
         </div>
         )
+    }
+
+    downloadJsonFile = () => {
+        const element = document.createElement("a");
+        var block = [];
+        const nbBlocks = this.state.scenes.length;
+        for (let j=0; j< nbBlocks; j++) {
+            block.push({
+                blockID: this.state.scenes[j].id,
+                blockName: this.state.scenes[j].titre,
+                blockContenu: this.state.scenes[j].texte
+            })
+            console.log(this.state.scenes[j])
+            const nbScenes = this.state.scenes[j].state.scene.scenesSuivantes.length;
+            var ListeChoix = [];
+            for (let i = 0; i < nbScenes; i++) {
+            ListeChoix.push({
+                ChoixTexte: this.state.scenes[j].state.scene.scenesSuivantes[i].texte,
+                ChoixIdSuivant: this.state.scenes[j].scene.scenesSuivantes[i].idSuivant
+            });
+
+            }
+            block["ListeChoix"] = ListeChoix;
+        }
+        
+        const file = new Blob([JSON.stringify(block, '\t', 2)], { type: 'application/json' });
+        element.href = URL.createObjectURL(file);
+        //element.download = "myFile.json";
+        document.body.appendChild(element); // Required for this to work in FireFox
+        element.click();
     }
 }
 
