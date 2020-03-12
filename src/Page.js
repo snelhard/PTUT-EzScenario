72afba5d8e5e6865068a52ac4009e4924f6fcfae
snelhard <43958032+snelhard@users.noreply.github.com>
2020-03-12 16:12:01 +0100

    good page

--- src/Page.js
+++ src/Page.js
@@ -9,18 +9,22 @@ class Page extends React.Component{
         ],
         IdScene:0
     }
+    
     addBlock = () => {
         var scenes = this.state.scenes.slice();
-        scenes.push({id: this.state.IdScene+1, titre: "", text: ""});
+        scenes.push({id: this.state.IdScene.length+1, titre: "", text: ""});
         this.state.IdScene+=1;
         this.setState({scenes: scenes});
     }
     render(){
+        
+       
         return(
         <div className="game">
+            
             <h1>{this.state.titre}</h1>
             <button onClick={this.addBlock}>Ajouter scene</button>
-            <button onClick={this.downloadJsonFile}>Fichier Json</button>
+            
             <div className="game-board">
                 {this.state.scenes.map((scene) => (
                     <Block key={scene.id}/>
