d7d426ddb5f8f75ca5367030e012f8287e4e66cd
TherapyBTTF <45455608+TherapyBTTF@users.noreply.github.com>
2020-05-26 15:54:36 +0200

    changement css

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.css';
+/*import 'bootstrap/dist/css/bootstrap.css';*/
+import '../contact.css';
 import axios from 'axios';
 
 class Contact extends React.Component {
@@ -37,41 +38,48 @@ class Contact extends React.Component {
 
   render() {
     return (
-      <Form onSubmit={this.handleSubmit} style={{ width: '800px' }}>
-        <FormGroup>
-          <Label for="name">Nom : </Label>
-          <Input
-            type="text"
-            name="name"
-            onChange={this.handleChange} />
-        </FormGroup>
+      <div>
+        <h1>Contactez-nous</h1>
+        <Form id="form" onSubmit={this.handleSubmit} style={{ width: '800px' }}>
+          <FormGroup>
+            <Label id="labels" for="name">Nom : </Label>
+            <Input
+              id="input"
+              type="text"
+              name="name"
+              onChange={this.handleChange} />
+          </FormGroup>
 
-        <FormGroup>
-          <Label for="Email">Email : </Label>
-          <Input
-            type="email"
-            name="email"
-            onChange={this.handleChange} />
-        </FormGroup>
+          <FormGroup>
+            <Label id="labels" for="Email">Email : </Label>
+            <Input
+              id="input"
+              type="email"
+              name="email"
+              onChange={this.handleChange} />
+          </FormGroup>
 
-        <FormGroup>
-          <Label for="Objet">Objet : </Label>
-          <Input
-            type="text"
-            name="objet"
-            onChange={this.handleChange} />
-        </FormGroup>
+          <FormGroup>
+            <Label id="labels" for="Objet">Objet : </Label>
+            <Input
+              id="input"
+              type="text"
+              name="objet"
+              onChange={this.handleChange} />
+          </FormGroup>
 
-        <FormGroup>
-          <Label for="name">Message : </Label>
-          <Input
-            type="textarea"
-            name="message"
-            onChange={this.handleChange} />
-        </FormGroup>
+          <FormGroup>
+            <Label id="labels" for="name">Message : </Label>
+            <Input
+              id="input"
+              type="textarea"
+              name="message"
+              onChange={this.handleChange} />
+          </FormGroup>
 
-        <Button>Envoyer</Button>
-      </Form >
+          <Button id="bouton">Envoyer</Button>
+        </Form >
+      </div>
     );
   }
 
