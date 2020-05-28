2f61e2fdcd4d0ecf2842cf88cef07e8994b622b7
snelhard <43958032+snelhard@users.noreply.github.com>
2020-05-28 10:38:41 +0200

    Correction bug actualisation champ choix

--- src/rete/Control.js
+++ src/rete/Control.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { Control } from "rete";
 import Rete from "rete";
 
 
@@ -28,8 +27,6 @@ class MyReactControl extends React.Component {
       )
     }
 
-    // console.log(Tab)
-
     this.setState({
       titre: this.props.getData('titre'),
       texte: this.props.getData('texte'),
@@ -38,23 +35,16 @@ class MyReactControl extends React.Component {
       typeof(this.props.getData('choix3')) == 'undefined' ? this.enFonctionDuNbChoix() : this.props.getData('choix3'),
       typeof(this.props.getData('choix4')) == 'undefined' ? this.enFonctionDuNbChoix() : this.props.getData('choix4'),
       typeof(this.props.getData('choix5')) == 'undefined' ? this.enFonctionDuNbChoix() : this.props.getData('choix5')],
-      //choix1: this.props.getData('choix1'),
-      //choix2: this.props.getData('choix2')
-    },
-    // console.log("liste choix "+this.props.getData("inputs"))
+     },
     );
 
     
   }
 
-  
-
   onChange(event) {
     // this.props.emitter.trigger("process");
     let target = event.target.name;
     let value = event.target.value;
-    // console.log(target + " " + value)
-    
     this.update(target, value);
   }
   
@@ -62,6 +52,11 @@ class MyReactControl extends React.Component {
     this.setState({
       [target]: value
     }, () => this.props.putData(target, value));
+
+    const numeroChoix = target.replace(/\D/g, "");
+    if (typeof(this.state.listeChoix) != 'undefined') {
+      this.state.listeChoix[numeroChoix-1] = value;
+    }
   }
 
   render() {
@@ -87,11 +82,8 @@ class MyReactControl extends React.Component {
     return (
       <div style={{color: "white  "}}>
         <label>Titre</label><input value={this.state.titre} name="titre" onChange={this.onChange.bind(this)} style={{"width" : "100%"}}/><br/>  
-        {/* <label>Texte</label><input value={this.state.texte} name="texte" onChange={this.onChange.bind(this)} /><br/> */}
         <label>Texte</label><textarea name="texte" onChange={this.onChange.bind(this)} value={this.state.texte} style={{"minHeight": "80px", "width" : "100%"}}/><br/>
         {lesChoix}
-        {/* <label>Choix 1</label><input value={this.state.choix1} name="choix1" onChange={this.onChange.bind(this)} style={{"width" : "100%"}}/><br/> */}
-        {/* <label>Choix 2</label><input value={this.state.choix2} name="choix2" onChange={this.onChange.bind(this)} style={{"width" : "100%"}}/> */}
       </div>
     );
   }
